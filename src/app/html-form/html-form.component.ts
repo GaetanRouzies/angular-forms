e2cd44ad60455c42a189fcd281f4e16ab8f292f3
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-07-02 21:44:14 +0200

    Add for and id

---
+++ src/app/html-form/html-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-html-form',
+  standalone: true,
+  templateUrl: './html-form.component.html',
+  imports: [],
+})
+export class HtmlFormComponent {
+
+}

91d11f326268a7b4a0736702f2a285c24f368033
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-07-03 21:34:44 +0200

    Add default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,11 @@ import { ReactiveForm3Component } from './reactive-form-3/reactive-form3.compone
 import { ReactiveForm4Component } from './reactive-form-4/reactive-form4.component';
 import { ReactiveFormValuesComponent } from './reactive-form-values/reactive-form-values.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { HtmlFormComponent } from './html-form/html-form.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/html-form', pathMatch: 'full' },
+  { path: 'html-form', component: HtmlFormComponent },
   { path: 'template-driven-form', component: TemplateDrivenFormComponent },
   { path: 'reactive-form-1', component: ReactiveForm1Component },
   { path: 'reactive-form-2', component: ReactiveForm2Component },
